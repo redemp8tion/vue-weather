23210d59dfc151b0e8ed5d615f37f862a5a9c00d
redem8ption <redem8ption@163.com>
2024-09-04 19:17:17 +0800

    搜索组件静态搭建

--- src/api/weather.js
+++ src/api/weather.js
@@ -1,5 +1,5 @@
 export const fetchWeatherData = async (city) => {
-    const apiKey = '1507272ff76d54316f3b285110d59a34';
+    const apiKey = '5ce61b107390ba58323d0f7b554a7625';
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
     const data = await response.json();
     if (data.cod !== 200) throw new Error(data.message);
---
+++ src/components/search/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="search">
+    <el-input style="width: 240px" size="small" placeholder="Search for places">
+      <template #prepend>
+        <el-button :icon="Search" />
+      </template>
+    </el-input>
+  </div>
+</template>
+
+<script setup>
+import { Search } from "@element-plus/icons-vue";
+</script>
+
+<style lang="scss" scoped>
+.search {
+  width: 100%;
+  height: 30px;
+  margin-top: 20px;
+  margin-left: 10px;
+}
+</style>
